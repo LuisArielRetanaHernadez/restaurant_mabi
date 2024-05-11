0b6293aec7b97970747ff2458f3c3d3a0cb423a0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-10 20:04:20 -0600

    creando el middleware para protejer las rutas que requieren autentificacion

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,21 @@
 import type { NextAuthConfig } from "next-auth";
 
+const pagesProtected: string[] = [""];
+
 export const authConfig = {
   pages: {
     signIn: "/login",
   },
-};
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      if (pagesProtected.includes(nextUrl.pathname)) {
+        if (!isLoggedIn) return true;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL(nextUrl.pathname, nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
