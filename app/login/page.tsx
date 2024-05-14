6201ea87c4698250818540653420a57acadda735
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-13 20:42:09 -0600

    adaptando el login a las diferentes resoluciones de pantallas

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,15 +9,15 @@ export default function PageLogin() {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <section className="w-full h-screen bg-white flex">
-      <div className="w-2/3 h-[400px] m-auto rounded-md shadow-2xl shadow-blue-800 overflow-hidden flex">
-        <form className="w-1/2 text-center p-1 m-auto">
+      <div className="w-5/6 md:w-2/3 h-[400px] m-auto rounded-md shadow-2xl shadow-blue-800 flex">
+        <form className="w-full md:w-1/2 text-center m-auto p-1 ">
           <h2 className="text-blue-800 text-4xl mb-6">Login</h2>
           <div className="mb-5">
             <input
               type="email"
               name="email"
               id="email"
-              className="w-full p-4 font-bold text-black text-md border-gray-200 rounded-lg outline-none"
+              className="w-full p-4 font-bold text-black text-md md:text-sm border-gray-200 rounded-lg outline-none"
               placeholder="Email address"
             />
           </div>
@@ -26,7 +26,7 @@ export default function PageLogin() {
               type={showPassword ? "text" : "password"}
               name="password"
               id="password"
-              className="w-full p-4 font-bold text-md text-black text-md rounded-lg outline-none"
+              className="w-full p-4 font-bold text-md md:text-sm text-black text-md rounded-lg outline-none"
               placeholder="Password"
             />
             {showPassword ? (
@@ -50,7 +50,7 @@ export default function PageLogin() {
             </button>
           </div>
         </form>
-        <div className="w-1/2 p-1 bg-blue-800 flex">
+        <div className="w-full md:w-1/2 h-full hidden md:flex p-1 bg-blue-800">
           <Link
             className="m-auto text-blue-800 py-2 px-3 rounded-md bg-white"
             href="register"
