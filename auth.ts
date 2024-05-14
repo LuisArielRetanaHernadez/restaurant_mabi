86e386022e443531fbbbc40aed8227d9f006d6b5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-14 14:22:51 -0600

    agregando la funcion fetchLoginEmplooye para consultar a la base de datos

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 
 import { z } from "zod";
+import { fetchLoginEmplooye } from "./app/lib/data";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -12,6 +13,15 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const employee = await fetchLoginEmplooye(email, password);
+          if (!employee) {
+            return null;
+          }
+          return null;
+        }
       },
     }),
   ],
