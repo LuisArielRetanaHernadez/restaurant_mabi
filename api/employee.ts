063e033c4552b7ec18f45420b9cd5cd929d606fd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-05-14 13:01:20 -0600

    creando el router api login para employee

---
+++ api/employee.ts
@@ -0,0 +1,34 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import prisma from "@/utils/connect";
+
+const bcrypt = require("bcrypt");
+export const loginApi = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const { email, password } = await req.json();
+
+    const emplooye = prisma.emplooye.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!emplooye)
+      return NextResponse.json({
+        status: "fail",
+        message: "invalid credentials",
+      });
+
+    const matchPassword = await bcrypt.compare(password, emplooye.password);
+
+    if (!matchPassword)
+      return NextResponse.json({
+        status: "fail",
+        message: "invalid credentials",
+      });
+
+    return NextResponse.json({ status: "success", emplooye });
+  } catch (error) {
+    return NextResponse.json({ status: "error", message: error });
+  }
+};
